f0a5da7f69dfd43b8bc1f42f5b07ebca67fb6ba1
Fabricio Clemente <144067212+Fabclemente@users.noreply.github.com>
2024-06-08 01:40:14 -0600

    toogle option added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
+
+interface Product {
+  name: string;
+  price: number;
+  image: string;
+  details: string;
+  showDetails: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tienda_Online';
+  products: Product[] = [
+    {
+      name: 'Producto 1',
+      price: 99,
+      image: 'https://via.placeholder.com/150',
+      details: 'Detalles del Producto 1',
+      showDetails: false
+    },
+    {
+      name: 'Producto 2',
+      price: 99,
+      image: 'https://via.placeholder.com/150',
+      details: 'Detalles del Producto 2',
+      showDetails: false
+    },
+    {
+      name: 'Producto 3',
+      price: 99,
+      image: 'https://via.placeholder.com/150',
+      details: 'Detalles del Producto 3',
+      showDetails: false
+    },
+    {
+      name: 'Producto 4',
+      price: 99,
+      image: 'https://via.placeholder.com/150',
+      details: 'Detalles del Producto 4',
+      showDetails: false
+    }
+  ];
+
+  addToCart(product: Product) {
+    // Lógica para agregar al carrito
+    console.log(`${product.name} agregado al carrito.`);
+  }
+
+  toggleDetails(product: Product) {
+    product.showDetails = !product.showDetails;
+  }
 }
+
